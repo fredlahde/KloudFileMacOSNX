b8836d45568babd581cf5c8ed189378db647e0d4
Fred Lahde <fredlahde@gmail.com>
2016-10-19 22:11:30 +0200

    init

--- KloudFileMacOSNX/AppDelegate.swift
+++ KloudFileMacOSNX/AppDelegate.swift
@@ -10,18 +10,39 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
+    
+    
+    @IBOutlet weak var statusMenu: NSMenu!
+    let statusItem = NSStatusBar.system().statusItem(withLength: -2)
+    
+    
+    @IBAction func screenshotClicked(_ sender: AnyObject) {
+        let task = Process()
+        
+        task.launchPath = "/usr/sbin/screencapture"
+        task.arguments = ["-i", "screen.png"]
+        
+        let pipe = Pipe()
+        task.standardOutput = pipe
+        task.launch()
 
-    @IBOutlet weak var window: NSWindow!
-
-
+    }
+    
+    @IBAction func quitClicked(_ sender: AnyObject) {
+        NSApplication.shared().terminate(self)
+    }
     func applicationDidFinishLaunching(_ aNotification: Notification) {
+        let icon = NSImage(named: "statusIcon")
+        icon?.isTemplate = true // best for dark mode
+        statusItem.image = icon
+        statusItem.menu = statusMenu
         // Insert code here to initialize your application
     }
-
+    
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-
-
+    
+    
 }
 
