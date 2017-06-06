d4f1e68cc6b1bdadffb95bab3e7e091377530e21
Fred Lahde <fredlahde@gmail.com>
2017-06-06 10:36:47 +0200

    format

--- KloudFileMacOSNX/AppDelegate.swift
+++ KloudFileMacOSNX/AppDelegate.swift
@@ -10,35 +10,34 @@ import Cocoa
 
 @NSApplicationMain
 class AppDelegate: NSObject, NSApplicationDelegate {
-    
-    let screenshotManager : ScreenshotManager = ScreenshotManager()
-    
+
+    let screenshotManager = ScreenshotManager()
+
     @IBOutlet weak var statusMenu: NSMenu!
     let statusItem = NSStatusBar.system().statusItem(withLength: -2)
-    
-    
+
+
     @IBAction func screenshotClicked(_ sender: AnyObject) {
         screenshotManager.takeScreenshot()
     }
-    
+
     @IBAction func quitClicked(_ sender: AnyObject) {
         NSApplication.shared().terminate(self)
     }
-    
+
     func applicationDidFinishLaunching(_ aNotification: Notification) {
         let icon = NSImage(named: "statusIcon")
         icon?.isTemplate = true // best for dark mode
         statusItem.image = icon
         statusItem.menu = statusMenu
-        
-             // Insert code here to initialize your application
+
+        // Insert code here to initialize your application
     }
-    
+
     func applicationWillTerminate(_ aNotification: Notification) {
         // Insert code here to tear down your application
     }
-    
-    
-    
+
+
 }
 
--- KloudFileMacOSNX/ScreenshotManager.swift
+++ KloudFileMacOSNX/ScreenshotManager.swift
@@ -11,87 +11,88 @@ import Cocoa
 import Alamofire
 
 
-class ScreenshotManager  {
-    
+class ScreenshotManager {
+    private let url = "https://kloudfile.io/api/post"
+
+
     func takeScreenshot() {
-        
+
 
         let filename = "/.screen.png"
         let url = NSURL.fileURL(withPath: NSHomeDirectory() + filename)
-        let task : Process = Process()
+        let task: Process = Process()
         task.launchPath = "/usr/sbin/screencapture"
         task.arguments = ["-i", url.path]
-        
-        let pipe : Pipe = Pipe()
+
+        let pipe: Pipe = Pipe()
         task.standardOutput = pipe
         task.launch()
         task.waitUntilExit()
-        
-        
+
+
         Swift.print("finished task")
-        
+
         uploadFile(path: url.path, key: getKey())
-        
+
     }
-    
-    func getKey() -> String {
+
+    private func getKey() -> String {
         let path = "/.key.json"
         let url = NSURL.fileURL(withPath: NSHomeDirectory() + path)
-        
+
         print(url.path)
         do {
-            if let jsonData = NSData(contentsOfFile: url.path)
-            {
+            if let jsonData = NSData(contentsOfFile: url.path) {
                 let jsonResult: NSDictionary = try JSONSerialization.jsonObject(with: jsonData as Data, options: JSONSerialization.ReadingOptions.mutableContainers) as! NSDictionary
-                
+
                 if let key = jsonResult.value(forKey: "key") as! String? {
                     return key;
                 }
-                
+
                 return ""
-                
+
             }
-            
+
         } catch {
             print("error")
-             return ""
+            return ""
         }
         return ""
     }
-    
-    func uploadFile(path: String, key: String) {
+
+    private func uploadFile(path: String, key: String) {
         var parameters = [String: String]()
         parameters["key"] = key
 
         Alamofire.upload(
-            multipartFormData: { multipartFormData in
-                for (key, value) in parameters {
-                    multipartFormData.append(value.data(using: .utf8)!, withName: key)
-                }
-                multipartFormData.append(NSURL.fileURL(withPath: path), withName: "file")
-        }, to: "https://kloudfile.io/api/post",
-            encodingCompletion: { encodingResult in
-                switch encodingResult {
-                case .success(let upload, _, _):
-  
-                    
-                    upload.responseJSON { response in
-                        if response.response!.statusCode != 200 {
-                            return
-                        }
-                        
-                        Swift.print("Image uploaded")
-                        let JSON = response.result.value as! NSDictionary
-                        let URL = "https://kloudfile.io" + (JSON.value(forKey: "fileViewUrl") as! String)
-                        let pasteboard = NSPasteboard.general()
-                        pasteboard.clearContents()
-                        pasteboard.setString(URL, forType: NSPasteboardTypeString)
-  
+                multipartFormData: { multipartFormData in
+                    for (key, value) in parameters {
+                        multipartFormData.append(value.data(using: .utf8)!, withName: key)
                     }
-                case .failure(let encodingError):
-                    print(encodingError)
+                    multipartFormData.append(NSURL.fileURL(withPath: path), withName: "file")
+                }, to: url,
+                encodingCompletion: { encodingResult in
+                    switch encodingResult {
+                    case .success(let upload, _, _):
+
+
+                        upload.responseJSON { response in
+                            if response.response!.statusCode != 200 {
+                                return
+                            }
+
+                            Swift.print("Image uploaded")
+                            let JSON = response.result.value as! NSDictionary
+                            let URL = "https://kloudfile.io" + (JSON.value(forKey: "fileViewUrl") as! String)
+                            let pasteboard = NSPasteboard.general()
+                            pasteboard.clearContents()
+                            pasteboard.setString(URL, forType: NSPasteboardTypeString)
+
+                        }
+                    case .failure(let encodingError):
+                        print(encodingError)
+                    }
                 }
-            }
         )
     }
 }
